bac3291d22fcee18b1d64e126b5c054672b9c2b4
Rockmiin <csm1959@naver.com>
2021-09-29 18:46:15 +0900

    add [sub] [obj] token

--- preprocess.py
+++ preprocess.py
@@ -37,60 +37,54 @@ class RE_Dataset(torch.utils.data.Dataset):
   def __len__(self):
     return len(self.labels)
 
+def load_data(data):
+  
+  sub_entity, sub_type= [], []
+  obj_entity, obj_type= [], []
+  sub_idx, obj_idx= [], []
+  sentence= []
 
-class Preprocess:
-  def __init__(self, CSV_PATH):
-    self.data= pd.read_csv(CSV_PATH)
-    self.df= self.load_data(self.data)
-
-
-  def load_data(self, data):
-    
-    sub_entity, sub_type= [], []
-    obj_entity, obj_type= [], []
-    sub_idx, obj_idx= [], []
-    sentence= []
-
-    for i, [x, y, z] in enumerate(zip(self.data['subject_entity'], self.data['object_entity'], self.data['sentence'])):
-        sub_typ= x[1:-1].split(':')[-1].split('\'')[-2]
-        obj_typ= y[1:-1].split(':')[-1].split('\'')[-2]
+  for i, [x, y, z] in enumerate(zip(data['subject_entity'], data['object_entity'], data['sentence'])):
+      sub_typ= x[1:-1].split(':')[-1].split('\'')[-2]
+      obj_typ= y[1:-1].split(':')[-1].split('\'')[-2]
+      
+      for idx_i in range(len(x)):
+        if x[idx_i: idx_i+ 9]== 'start_idx':
+          sub_start= int(x[idx_i+12:].split(',')[0].strip())
+        if x[idx_i: idx_i+7]== 'end_idx':
+          sub_end= int(x[idx_i+10:].split(',')[0].strip())
         
-        for idx_i in range(len(x)):
-          if x[idx_i: idx_i+ 9]== 'start_idx':
-            sub_start= int(x[idx_i+12:].split(',')[0].strip())
-          if x[idx_i: idx_i+7]== 'end_idx':
-            sub_end= int(x[idx_i+10:].split(',')[0].strip())
-          
-          if y[idx_i: idx_i+ 9]== 'start_idx':
-            obj_start= int(y[idx_i+12:].split(',')[0].strip())
-          if y[idx_i: idx_i+7]== 'end_idx':
-            obj_end= int(y[idx_i+10:].split(',')[0].strip())
-        
-        sub_i= [sub_start, sub_end]
-        obj_i= [obj_start, obj_end]
+        if y[idx_i: idx_i+ 9]== 'start_idx':
+          obj_start= int(y[idx_i+12:].split(',')[0].strip())
+        if y[idx_i: idx_i+7]== 'end_idx':
+          obj_end= int(y[idx_i+10:].split(',')[0].strip())
+      
+      sub_i= [sub_start, sub_end]
+      obj_i= [obj_start, obj_end]
 
-        sub_entity.append(z[sub_i[0]: sub_i[1]+1])
-        obj_entity.append(z[obj_i[0]: obj_i[1]+1])
-        sub_type.append(sub_typ); sub_idx.append(sub_i)
-        obj_type.append(obj_typ); obj_idx.append(obj_i)
+      sub_entity.append(z[sub_i[0]: sub_i[1]+1])
+      obj_entity.append(z[obj_i[0]: obj_i[1]+1])
+      sub_type.append(sub_typ); sub_idx.append(sub_i)
+      obj_type.append(obj_typ); obj_idx.append(obj_i)
 
-        if sub_i[0] < obj_i[0]:
-          z= z[:sub_i[0]] + '[SUB]'+ z[sub_i[0]: sub_i[1]+1] + '[SUB]' + z[sub_i[1]+1:]
-          z= z[:obj_i[0]+10] + '[OBJ]'+ z[obj_i[0]+10: obj_i[1]+11]+ '[OBJ]'+ z[obj_i[1]+15:]
-        else:
-          z= z[:obj_i[0]] + '[OBJ]'+ z[obj_i[0]: obj_i[1]+1]+ '[OBJ]'+ z[obj_i[1]+1:]
-          z= z[:sub_i[0]+10] + '[SUB]'+ z[sub_i[0]+10: sub_i[1]+11] + '[SUB]' + z[sub_i[1]+15:]
+      if sub_i[0] < obj_i[0]:
+        z= z[:sub_i[0]] + '[SUB]'+ z[sub_i[0]: sub_i[1]+1] + '[SUB]' + z[sub_i[1]+1:]
+        z= z[:obj_i[0]+10] + '[OBJ]'+ z[obj_i[0]+10: obj_i[1]+11]+ '[OBJ]'+ z[obj_i[1]+15:]
+      else:
+        z= z[:obj_i[0]] + '[OBJ]'+ z[obj_i[0]: obj_i[1]+1]+ '[OBJ]'+ z[obj_i[1]+1:]
+        z= z[:sub_i[0]+10] + '[SUB]'+ z[sub_i[0]+10: sub_i[1]+11] + '[SUB]' + z[sub_i[1]+15:]
 
 
-        sentence.append(z)
+      sentence.append(z)
 
-    df= pd.DataFrame({'id': self.data['id'], 'sentence' : sentence, 'subject_entity': sub_entity, 'object_entity': obj_entity,
-                            'subject_type': sub_type, 'object_type': obj_type, 'label': self.data['label'],
-                            'subject_idx': sub_idx, 'object_idx': obj_idx})
+  df= pd.DataFrame({'id': data['id'], 'sentence' : sentence, 'subject_entity': sub_entity, 'object_entity': obj_entity,
+                          'subject_type': sub_type, 'object_type': obj_type, 'label': data['label'],
+                          'subject_idx': sub_idx, 'object_idx': obj_idx})
 
-    for i in range(10):    
-      print(f"SUB : {df.loc[i]['subject_entity']}\nOBJ : {df.loc[i]['object_entity']}\nSENTENCE : {df.loc[i]['sentence']}\n\n")
-    return df
+  for i in range(10):    
+    print(f"SUB : {df.loc[i]['subject_entity']}\nOBJ : {df.loc[i]['object_entity']}\nSENTENCE : {df.loc[i]['sentence']}\n\n")
+  
+  return df
 
 
 
@@ -98,7 +92,8 @@ if __name__ == '__main__':
 
   TRAIN_PATH= '/opt/ml/dataset/train/train.csv'
 
-  preprocess= Preprocess(TRAIN_PATH)
+  data= pd.read_csv(TRAIN_PATH)
+  load_data(data)
   # print(preprocess.df)
   # preprocess.save_tokenizing()
   # preprocess.tokenized_dataset()
